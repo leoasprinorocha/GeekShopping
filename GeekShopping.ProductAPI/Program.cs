9c05917579227091c6bcded8813d6c566dea2c91
Leonardo Asprino <leonardo.rocha@auvo.com.br>
2023-03-13 22:32:58 -0300

    Add project files.

---
+++ GeekShopping.ProductAPI/Program.cs
@@ -0,0 +1,48 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Models.Context;
+using GeekShopping.ProductAPI.Repository.Interfaces;
+using GeekShopping.ProductAPI.Repository.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+#region Repositories
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+#endregion
+
+
+var connection = builder.Configuration["MySQlConnection:MySQlConnectionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options =>
+{
+    options.UseMySql(connection,
+                    new MySqlServerVersion(new Version(5, 7, 20)));
+
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ GeekShopping.ProductAPI/Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,69 @@
+﻿using AutoMapper;
+using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Models;
+using GeekShopping.ProductAPI.Models.Context;
+using GeekShopping.ProductAPI.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Repository.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly MySqlContext _context;
+        private readonly IMapper _mapper;
+
+
+        public ProductRepository(IMapper mapper, MySqlContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        public async Task<ProductVO> Create(ProductVO vo)
+        {
+            Product product = _mapper.Map<Product>(vo);
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<bool> Delete(long id)
+        {
+            try
+            {
+                Product product = _mapper.Map<Product>(await FindById(id));
+                if (product is null)
+                    return false;
+                _context.Products.Remove(product);
+                await _context.SaveChangesAsync();
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<ProductVO>> FindAll()
+        {
+            List<Product> products = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductVO>>(products);
+        }
+
+        public async Task<ProductVO> FindById(long id)
+        {
+            Product product = await _context.Products.Where(c => c.Id == id).FirstOrDefaultAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+
+        public async Task<ProductVO> Update(ProductVO vo)
+        {
+            Product product = _mapper.Map<Product>(vo);
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+            return _mapper.Map<ProductVO>(product);
+        }
+    }
+}
